fcc2fc74a1e2825409cfc954c710b19e0a4daacb
Faizaan Khan <faizaankhan.us@gmail.com>
2023-04-23 15:17:56 +0200

    remove apostrophe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Faizaan's Portfolio</title>
+        <title>Porfolio of Faizaa Khan</title>
       </Head>
       <Component {...pageProps} />
     </>
